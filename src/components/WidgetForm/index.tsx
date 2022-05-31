80dd81fc96ffaac737a6e450a4a0f6564ee353b9
Claudio Lins <claudio.lins@me.com>
2022-05-31 08:27:11 +0100

    first commit

---
+++ src/components/WidgetForm/index.tsx
@@ -0,0 +1,68 @@
+import { Heart } from "phosphor-react";
+import { useState } from "react";
+import bugImageUrl from "../../../public/button-card/bug.svg";
+import ideiaImageUrl from "../../../public/button-card/ideia.svg";
+import thoughtImageUrl from "../../../public/button-card/thought.svg";
+import { CloseButton } from "../CloseButton";
+import { FeedbackContentStep } from "./Steps/FeedbackContentStep";
+import { FeedbackSuccessStep } from "./Steps/FeedbackSuccessStep";
+import { FeedbackTypeStep } from "./Steps/FeedbackTypeStep";
+
+export const feedbackTypes = {
+  BUG: {
+    title: "Problemas",
+    image: {
+      source: bugImageUrl,
+      alt: "Imagem de um inseto",
+    },
+  },
+  IDEA: {
+    title: "Ideia",
+    image: {
+      source: ideiaImageUrl,
+      alt: "Imagem de uma lampada",
+    },
+  },
+  OTHER: {
+    title: "Outro",
+    image: {
+      source: thoughtImageUrl,
+      alt: "Imagem de um balão de pensamento",
+    },
+  },
+};
+
+export type FeedbackType = keyof typeof feedbackTypes;
+
+export function WidgetForm() {
+  const [feedbackType, setFeedbackType] = useState<FeedbackType | null>(null);
+  const [feedbackSent, setFeedbackSent] = useState(false);
+
+  function handleRestartFeedback() {
+    setFeedbackSent(false)
+    setFeedbackType(null);
+  }
+
+  return (
+    <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
+      {feedbackSent ? (
+        <FeedbackSuccessStep onFeedbackRestartRequested={handleRestartFeedback} />
+      ) : (
+        <>
+          {!feedbackType ? (
+            <FeedbackTypeStep onFeedbackTypeChanged={setFeedbackType} />
+          ) : (
+            <FeedbackContentStep
+              feedbackType={feedbackType}
+              onFeedbackRestartRequest={handleRestartFeedback}
+              onFeedbackSent={() => setFeedbackSent(true)}
+            />
+          )}
+        </>
+      )}
+      <footer className="text-xs flex gap-2 justify-center items-center text-neutral-400">
+        Feito com {<Heart size={12} />} pelo Claudio Lins
+      </footer>
+    </div>
+  );
+}
